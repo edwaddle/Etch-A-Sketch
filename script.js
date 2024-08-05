bf0ef89a8aa8ac8c15e6cfb07e7b93657aa61ca7
edwin yue <53625790+edwaddle@users.noreply.github.com>
2024-08-04 17:57:46 -0700

    Added a reset button
    Uses onclick to set count(num of boxes per row), and then add all the boxes in.

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 let container = document.querySelector(".outsideSquare");
 
-
+let reset = document.querySelector(".reset");
 let count = 8;
-let containerWidth = container.offsetWidth;
-console.log(containerWidth);
-let sizing = (containerWidth/count)-1;
-console.log(sizing);
-for (let i = 0; i < count; i++){
-    for (let j = 0; j < count; j++){
-        let box = document.createElement("div");
-        box.style.height = sizing + "px";
-        box.style.width = sizing + "px";
-        box.setAttribute("class","box");
-        box.textContent = j;
-        container.appendChild(box);
+let box;
+reset.onclick = () => {
+    count = clickAlert()
+    if (box != null) { box.remove();} 
+    let containerWidth = container.offsetWidth;
+    console.log(containerWidth);
+    let sizing = (containerWidth/count)-1;
+    console.log(sizing);
+    for (let i = 0; i < count; i++){
+        for (let j = 0; j < count; j++){
+            box = document.createElement("div");
+            box.style.height = sizing + "px";
+            box.style.width = sizing + "px";
+            box.setAttribute("class","box");
+            box.textContent = j+1;
+            container.appendChild(box);
+        }
     }
+};
+function clickAlert(num ){
+    num = prompt("How mant boxes per row?");
+    console.log(num);
+    return num;
 }
+
+
+
+
+
+
+
+
 
